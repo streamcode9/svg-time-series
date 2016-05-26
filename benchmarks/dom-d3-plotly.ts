e89567d140b1e9f764194e2961027484fd27d46e
Alex <streamcode9@gmail.com>
2016-05-27 00:25:41 +0300

    Internal: plot.ly chart #7

---
+++ benchmarks/dom-d3-plotly.ts
@@ -0,0 +1,17 @@
+﻿declare var Plotly: any
+
+function generateData() {
+	let lines = []
+	for (let i = 0; i < 10; i++) {
+		var xs = [], ys = []
+		for (let x = 0; x < 5000; x++) {
+			xs.push(x * 0.2)
+			ys.push(f(x) * 100 + i * 50)
+		}
+		lines.push({ y: ys, x: xs })
+	}
+	return lines
+}
+
+var container = document.getElementById('plotly-container');
+Plotly.plot(container, generateData())
