f3fa1b9fd270a4af08322d63899b0a2e2b53469f
Alex <streamcode9@gmail.com>
2016-05-28 02:27:28 +0300

    Internal: benchmark first render #7

--- benchmarks/dom-d3-plotly.ts
+++ benchmarks/dom-d3-plotly.ts
@@ -6,12 +6,23 @@ function generateData() {
 		var xs = [], ys = []
 		for (let x = 0; x < 5000; x++) {
 			xs.push(x * 0.2)
-			ys.push(f(x) * 100 + i * 50)
+			ys.push(f(x) * 100 + 50 * i)
 		}
-		lines.push({ y: ys, x: xs })
+		lines.push({ y: ys, x: xs, line: { width: 1 }, showlegend: false })
 	}
 	return lines
 }
 
 var container = document.getElementById('plotly-container');
-Plotly.plot(container, generateData())
+
+let start = Date.now()
+const layout = {
+	autosize: false, width: 1270, height: 750,
+	yaxis: { showgrid: false, showticklabels: false },
+	xaxis: { showgrid: false, showticklabels: false }
+}
+function render() {
+	Plotly.plot(container, generateData(), layout, { staticPlot: true })
+	window.requestAnimationFrame(() => console.log(Date.now() - start))
+}
+window.requestAnimationFrame(render)
