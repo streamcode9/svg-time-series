36bdf6eef21f5ed9977ce7e22797679087bf0d95
Alex <streamcode9@gmail.com>
2016-06-18 16:24:38 +0300

    Internal: Display five pairs of charts Closes #27

--- benchmarks/d3-myaxis-ts/index.ts
+++ benchmarks/d3-myaxis-ts/index.ts
@@ -16,6 +16,16 @@ namespace Chart {
 		return (d: any) => scale(d) + width
 	}
 
+	function translateX(scale0: any, scale1: any, d: any) {
+		var x = scale0(d)
+		return 'translate(' + (isFinite(x) ? x : scale1(d)) + ',0)'
+	}
+
+	function translateY(scale0: any, scale1: any, d: any) {
+		var y = scale0(d)
+		return 'translate(0,' + (isFinite(y) ? y : scale1(d)) + ')'
+	}
+
 	class MyAxis {
 		tickArguments: any[]
 		tickValues: any
@@ -25,6 +35,7 @@ namespace Chart {
 		tickPadding: number
 		orient: number
 		scale: any
+
 		constructor(orient: any, scale: any) {
 			this.orient = orient
 			this.scale = scale
@@ -35,6 +46,7 @@ namespace Chart {
 			this.tickSizeOuter = 6
 			this.tickPadding = 3
 		}
+
 		axis(context: any) {
 			let values: any = this.tickValues == null ? (this.scale.ticks ? this.scale.ticks.apply(this.scale, this.tickArguments) : this.scale.domain()) : this.tickValues,
 				format: any = this.tickFormat == null ? (this.scale.tickFormat ? this.scale.tickFormat.apply(this.scale, this.tickArguments) : identity) : this.tickFormat,
@@ -135,19 +147,104 @@ namespace Chart {
 		}
 	}
 
-	function translateX(scale0: any, scale1: any, d: any) {
-		var x = scale0(d)
-		return 'translate(' + (isFinite(x) ? x : scale1(d)) + ',0)'
+	function drawChart(id: number, data: any) {
+		let svg = d3.select('#chart-' + id),
+			width = +svg.attr('width'),
+			height = +svg.attr('height')
+
+		let x = d3.scaleTime().range([0, width])
+		let y = d3.scaleLinear().range([height, 0])
+		let color = d3.scaleOrdinal().domain(['NY', 'SF']).range(['green', 'blue'])
+
+		var xAxis = new MyAxis(bottom, x)
+			.ticks((width + 2) / (height + 2) * 10)
+			.setTickSize(height)
+			.setTickPadding(8 - height)
+
+		var yAxis = new MyAxis(right, y)
+			.ticks(10)
+			.setTickSize(width)
+			.setTickPadding(8 - width)
+
+		let line = d3.line()
+			.x((d: any) => x(d.date))
+			.y((d: any) => y(d.value))
+
+		var cities = color.domain()
+			.map((name: string) => {
+				return ({
+					name: name,
+					values: data.filter((d: any) => !isNaN(d[name])).map((d: any) => ({ date: d.date, value: +d[name] }))
+				})
+			})
+
+		x.domain(d3.extent(data, (d: any) => d.date))
+		y.domain([
+			d3.min(cities, (c: any) => d3.min(c.values, (v: any) => v.value)),
+			d3.max(cities, (c: any) => d3.max(c.values, (v: any) => v.value))
+		])
+
+		var view = svg.selectAll('.view')
+			.data(cities)
+			.enter().append('g')
+			.attr('class', 'view')
+
+		view.append('path')
+			.attr('d', (d: any) => line(d.values))
+			.attr('stroke', (d: any) => color(d.name))
+
+		var gX = svg.append('g')
+			.attr('class', 'axis')
+			.call(xAxis.axis.bind(xAxis))
+
+		var gY = svg.append('g')
+			.attr('class', 'axis')
+			.call(yAxis.axis.bind(yAxis))
+
+		svg.append('rect')
+			.attr('class', 'zoom')
+			.attr('width', width)
+			.attr('height', height)
+			.call(d3.zoom()
+				.scaleExtent([1, 40])
+				.translateExtent([[-100, -100], [width + 90, height + 100]])
+				.on('zoom', zoomed))
+
+		let newZoom: any = null
+		let rx: any = null
+		let ry: any = null
+
+		let draw = drawProc(function () {
+			view.attr('transform', newZoom)
+			xAxis.setScale(rx).axisUp(gX)
+			yAxis.setScale(ry).axisUp(gY)
+		})
+
+		function zoomed() {
+			let z = d3.event.transform.toString()
+			if (z != newZoom) {
+				rx = d3.event.transform.rescaleX(x)
+				ry = d3.event.transform.rescaleY(y)
+				newZoom = z
+				draw()
+			}
+		}
+
+		function drawProc(f: any) {
+			let requested = false
+
+			return function () {
+				if (!requested) {
+					requested = true
+					d3.timeout(function (time: any) {
+						requested = false
+						f(time)
+					})
+				}
+			}
+		}
 	}
 
-	function translateY(scale0: any, scale1: any, d: any) {
-		var y = scale0(d)
-		return 'translate(0,' + (isFinite(y) ? y : scale1(d)) + ')'
-	}
-
-	var svg = d3.select('svg'),
-		width = +svg.attr('width'),
-		height = +svg.attr('height')
 	d3
 		.csv('ny-vs-sf.csv')
 		.row((d: any) => ({
@@ -156,102 +253,8 @@ namespace Chart {
 			SF: parseFloat(d.SF.split(';')[0])
 		}))
 		.get((error: any, data: any) => {
-			if (error != null) {
-				alert('Data can\'t be downloaded or parsed')
-				return
-			}
-
-			let x = d3.scaleTime().range([0, width])
-			let y = d3.scaleLinear().range([height, 0])
-			let color = d3.scaleOrdinal().domain(['NY', 'SF']).range(['green', 'blue'])
-
-			var xAxis = new MyAxis(bottom, x)
-				.ticks((width + 2) / (height + 2) * 10)
-				.setTickSize(height)
-				.setTickPadding(8 - height)
-
-			var yAxis = new MyAxis(right, y)
-				.ticks(10)
-				.setTickSize(width)
-				.setTickPadding(8 - width)
-
-			let line = d3.line()
-				.x((d: any) => x(d.date))
-				.y((d: any) => y(d.value))
-
-			var cities = color.domain()
-				.map((name: string) => {
-					return ({
-						name: name,
-						values: data.filter((d: any) => !isNaN(d[name])).map((d: any) => ({ date: d.date, value: +d[name] }))
-					})
-				})
-
-			x.domain(d3.extent(data, (d: any) => d.date))
-			y.domain([
-				d3.min(cities, (c: any) => d3.min(c.values, (v: any) => v.value)),
-				d3.max(cities, (c: any) => d3.max(c.values, (v: any) => v.value))
-			])
-
-			var view = svg.selectAll('.view')
-				.data(cities)
-				.enter().append('g')
-				.attr('class', 'view')
-
-			view.append('path')
-				.attr('d', (d: any) => line(d.values))
-				.attr('stroke', (d: any) => color(d.name))
-
-			var gX = svg.append('g')
-				.attr('class', 'axis')
-				.call(xAxis.axis.bind(xAxis))
-
-			var gY = svg.append('g')
-				.attr('class', 'axis')
-				.call(yAxis.axis.bind(yAxis))
-
-			svg.append('rect')
-				.attr('class', 'zoom')
-				.attr('width', width)
-				.attr('height', height)
-				.call(d3.zoom()
-					.scaleExtent([1, 40])
-					.translateExtent([[-100, -100], [width + 90, height + 100]])
-					.on('zoom', zoomed))
-
-			let newZoom: any = null
-			let rx: any = null
-			let ry: any = null
-
-			let draw = drawProc(function () {
-				view.attr('transform', newZoom)
-				xAxis.setScale(rx).axisUp(gX)
-				yAxis.setScale(ry).axisUp(gY)
-			})
-
-			function zoomed() {
-				let z = d3.event.transform.toString()
-				if (z != newZoom) {
-					rx = d3.event.transform.rescaleX(x)
-					ry = d3.event.transform.rescaleY(y)
-					newZoom = z
-					draw()
-				}
-			}
-
-			function drawProc(f: any) {
-				let requested = false
-
-				return function () {
-					if (!requested) {
-						requested = true
-						d3.timeout(function (time: any) {
-							requested = false
-							f(time)
-						})
-					}
-				}
-			}
+			if (error != null) alert('Data can\'t be downloaded or parsed')
+			else[0, 1, 2, 3, 4].forEach(i => drawChart(i, data))
 		})
 
 	function measureFPS(sec: any, drawFPS: any) {
