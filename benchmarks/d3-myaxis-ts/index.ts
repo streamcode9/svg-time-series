ba45fafb8c44cc002559fcf09b8af7bf65109dd8
Alex <streamcode9@gmail.com>
2016-07-10 23:53:55 +0300

    Internal: Real-time updates Closes #28

--- benchmarks/d3-myaxis-ts/index.ts
+++ benchmarks/d3-myaxis-ts/index.ts
@@ -30,7 +30,7 @@ namespace Chart {
 			.setTickPadding(8 - width)
 
 		let line = d3.line()
-			.x((d: number, i: number) => x(calcDate(i)))
+			.x((d: number, i: number) => x(calcDate(i, minX)))
 			.y((d: number) => y(d))
 
 		let cities = color.domain()
@@ -44,10 +44,7 @@ namespace Chart {
 		x.domain([minX, maxX])
 		y.domain(d3.extent(d3.merge(cities.map((v: any) => v.values))))
 
-		const clipWidth = x(calcDate(1))
-		svg.append('defs').append('clipPath').attr('id', 'clip').append('rect').attr('width', width - clipWidth).attr('height', height)
-
-		var view = svg.append('g').attr('clip-path', 'url(#clip)')
+		var view = svg.append('g')
 			.selectAll('.view')
 			.data(cities)
 			.enter().append('g')
@@ -74,7 +71,9 @@ namespace Chart {
 				.translateExtent([[-100, -100], [width + 90, height + 100]])
 				.on('zoom', zoomed))
 
-		charts.push({ x: x, y: y, xAxis: xAxis, yAxis: yAxis, gX: gX, gY: gY, view: view, data: cities, height: height, line: line, color: color })
+		charts.push({ x: x, y: y, rx: x.copy(), ry: y.copy(),
+			xAxis: xAxis, yAxis: yAxis, gX: gX, gY: gY, 
+			view: view, data: cities, height: height, line: line, color: color })
 	}
 
 	let newZoom: any = null
@@ -97,7 +96,7 @@ namespace Chart {
 				let domainX = chart.rx.domain()
 				let dataY = chart.data
 					.map((d: any) => d.values
-						.filter((v: any, i: number) => calcDate(i) >= domainX[0].getTime() && calcDate(i) <= domainX[1].getTime())
+						.filter((v: any, i: number) => calcDate(i, minX) >= domainX[0].getTime() && calcDate(i, minX) <= domainX[1].getTime())
 						.map((v: number) => v))
 				let domainY = d3.extent(d3.merge(dataY))
 				let newRangeY = [chart.y(domainY[0]), chart.y(domainY[1])]
@@ -116,20 +115,21 @@ namespace Chart {
 	function updateChartWithNewData(acc: number, cnt: number) {
 		if (acc == cnt) return
 
-		minX = calcDate(1)
-		maxX = calcDate(charts[0].data[0].values.length)
+		minX = calcDate(1, minX)
+		maxX = calcDate(charts[0].data[0].values.length - 1, minX)
+
+		const minimumRX = new Date(charts[0].rx.domain()[0].getTime() + stepX)
+		const maximumRX = new Date(charts[0].rx.domain()[1].getTime() + stepX)
 
 		charts.forEach((chart: any) => {
 			chart.data[0].values.push(chart.data[0].values[0])
 			chart.data[1].values.push(chart.data[1].values[0])
+
 			chart.x.domain([minX, maxX])
-			chart.view.selectAll('path')
-				.attr('d', (d: any) => chart.line(d.values))
-				.attr('stroke', (d: any) => chart.color(d.name))
-				.attr('class', (d: any) => d.name)
-				.attr('transform', null)
-			chart.xAxis.axisUp(chart.gX)
-			chart.view.selectAll('path').attr('transform', 'translate(-' + chart.x(calcDate(1)) + ', 0)')
+			chart.view.selectAll('path').attr('d', (d: any) => chart.line(d.values))
+
+			chart.rx.domain([minimumRX, maximumRX])
+			chart.xAxis.setScale(chart.rx).axisUp(chart.gX)
 		})
 		
 		d3.timeout(() => {
@@ -148,17 +148,18 @@ namespace Chart {
 			SF: parseFloat(d.SF.split(';')[0])
 		}))
 		.get((error: any, data: any) => {
-			minX = new Date()
-			maxX = calcDate(data.length - 1)
 			if (error != null) alert('Data can\'t be downloaded or parsed')
 			else {
+				minX = new Date()
+				maxX = calcDate(data.length - 1, minX);
+
 				[0, 1, 2, 3, 4].forEach((i: any) => drawChart(i, data))
 				updateChartWithNewData(1, 30)
 			}
 		})
 
-	function calcDate(index: number) {
-		return new Date(index*stepX + minX.getTime())
+	function calcDate(index: number, offset: Date) {
+		return new Date(index*stepX + offset.getTime())
 	}
 
 	measureFPS.measure(3, function (fps: any) {
