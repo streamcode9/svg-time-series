00b2e1832c9b05daa8fc8633466d152531540bc3
Alexander Slis <streamcode9@gmail.com>
2016-12-21 17:02:27 +0200

    Refactor: minor improvements

--- segmentTree.ts
+++ segmentTree.ts
@@ -1,4 +1,4 @@
-﻿function buidMinMaxTuple(fst: [number, number], snd: [number, number]): [number, number] {
+function buidMinMaxTuple(fst: [number, number], snd: [number, number]): [number, number] {
 	return [Math.min(fst[0], snd[0]), Math.max(fst[1], snd[1])]
 }
 
@@ -9,11 +9,11 @@ export class SegmentTree {
 	constructor(data: number[]) {
 		this.size = data.length
 		this.tree = new Array(this.size * 4)
-
-		let build = function (tree: [number, number][], values: number[], i: number, tl: number, tr: number) {
+		
+		const build = (tree: [number, number][], values: number[], i: number, tl: number, tr: number) => {
 			if (tl == tr) tree[i] = [values[tl], values[tl]]
 			else {
-				let tm = Math.floor((tl + tr) / 2)
+				const tm = Math.floor((tl + tr) / 2);
 				build(tree, values, 2 * i, tl, tm)
 				build(tree, values, 2 * i + 1, tm + 1, tr)
 				tree[i] = buidMinMaxTuple(tree[2 * i], tree[2 * i + 1])
@@ -22,30 +22,30 @@ export class SegmentTree {
 		build(this.tree, data, 1, 0, this.size - 1)
 	}
 
-	getMinMax(l: number, r: number) {
-		let min = function (tree: [number, number][], i: number, tl: number, tr: number, l: number, r: number): [number, number] {
+	getMinMax(from: number, to: number) {
+		const min = (tree: [number, number][], i: number, tl: number, tr: number, l: number, r: number): [number, number] => {
 			if (l > r) return [Infinity, -Infinity]
 
 			if (l == tl && r == tr) return tree[i]
 
-			let tm = Math.floor((tl + tr) / 2)
+			const tm = Math.floor((tl + tr) / 2);
 			return buidMinMaxTuple(
 				min(tree, i * 2, tl, tm, l, Math.min(r, tm)),
 				min(tree, i * 2 + 1, tm + 1, tr, Math.max(l, tm + 1), r))
 		}
-		return min(this.tree, 1, 0, this.size - 1, l, r)
+		return min(this.tree, 1, 0, this.size - 1, from, to)
 	}
 
 	update(position: number, newValue: [number, number]) {
-		let update = function(tree: [number, number][], i: number, tl: number, tr: number, pos: number, newTuple: [number, number]) {
+		const update = (tree: [number, number][], i: number, tl: number, tr: number, pos: number, newTuple: [number, number]) => {
 			if (tl == tr) {
 				tree[i] = newTuple
 				return
 			}
 
-			let tm = Math.floor((tl + tr) / 2)
-			if (pos <= tm) update (tree, i * 2, tl, tm, pos, newTuple)
-			else update (tree, i * 2 + 1, tm + 1, tr, pos, newTuple)
+			const tm = Math.floor((tl + tr) / 2);
+			if (pos <= tm) update(tree, i * 2, tl, tm, pos, newTuple)
+			else update(tree, i * 2 + 1, tm + 1, tr, pos, newTuple)
 			tree[i] = buidMinMaxTuple(tree[i * 2], tree[i * 2 + 1])
 		}
 		update(this.tree, 1, 0, this.size - 1, position, newValue)
