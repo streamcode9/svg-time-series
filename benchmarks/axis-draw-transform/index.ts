7670d42acaa92de8425bf75c63d5ad51572dde89
Alex <streamcode9@gmail.com>
2017-01-20 20:39:51 +0200

    Internal: remove timeout in benchmark

--- benchmarks/axis-draw-transform/index.ts
+++ benchmarks/axis-draw-transform/index.ts
@@ -29,7 +29,7 @@ function drawCharts(data: any[]) {
 	d3.interval(() => {
 		var t = d3.zoomIdentity.translate(getRandom(-500, 500), getRandom(-500, 500)).scale(getRandom(1, 40))
 		charts.forEach(c => c.zoom(t))
-	}, 500)
+	})
 }
 
 d3
