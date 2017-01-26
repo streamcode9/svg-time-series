5f7e417ad075f4dd13d51bc33cd9bfdca1a5803c
Alex <streamcode9@gmail.com>
2017-01-26 21:28:53 +0200

    New: update axis-draw-transform according to path-draw-transform-d3 optimizations #72

--- benchmarks/axis-draw-transform/draw.ts
+++ benchmarks/axis-draw-transform/draw.ts
@@ -1,123 +1,37 @@
 ﻿declare const require: Function
+
 const d3 = require('d3')
-import axis = require('../../axis')
-import segmentTree = require('../../segmentTree')
-
-interface IChartData {
-	name: string
-	values: number[]
-}
-
-interface IChartParameters {
-	x: Function
-	y: Function 
-	rx: Function
-	ry: Function
-	xAxis: axis.MyAxis
-	yAxis: axis.MyAxis
-	gX: any
-	gY: any
-	view: any
-	data: IChartData[]
-	height: number
-	color: Function
-}
-
-function drawProc(f: any) {
-	let requested = false
-
-	return function (...params: any[]) {
-		if (!requested) {
-			requested = true
-			d3.timeout(function (time: any) {
-				requested = false
-				f(params)
-			})
-		}
-	}
-}
+import d3timer = require('d3-timer')
+import { Selection, BaseType } from 'd3-selection'
+import { Orientation, MyAxis } from '../../axis'
 
 export class TimeSeriesChart {
-	private chart: IChartParameters
-	private minX: Date
-	private maxX: Date
-	private missedStepsCount: number
-	private stepX: number
-	private tree: segmentTree.SegmentTree
-	private buildSegmentTreeTuple: (index: number, elements: any) => segmentTree.IMinMax
+	constructor(
+		svg: Selection<BaseType, {}, HTMLElement, any>,
+		dataLength: number)
+	{
+		const node: SVGSVGElement = <SVGSVGElement>svg.node()
+		const div: HTMLElement = <HTMLElement>node.parentNode
 
-	constructor(svg: any, minX: Date, stepX: number, data: any[], buildSegmentTreeTuple: (index: number, elements: any) => segmentTree.IMinMax) {
-		this.stepX = stepX
-		this.minX = minX
-		this.maxX = this.calcDate(data.length - 1, minX)
-		this.buildSegmentTreeTuple = buildSegmentTreeTuple
-
-		this.drawChart(svg, data)
-
-		this.missedStepsCount = 0
-	}
-
-	public zoom = drawProc(function(param: any[]) {
-		const zoomTransform = param[0]
-		d3.zoom().transform(d3.selectAll('.zoom'), zoomTransform)
-		const translateX = zoomTransform.x
-		const scaleX = zoomTransform.k
-
-		this.chart.rx = zoomTransform.rescaleX(this.chart.x)
-		const domainX = this.chart.rx.domain()
-		const ySubInterval = this.getZoomIntervalY(domainX, this.chart.data[0].values.length)
-		const minMax = this.tree.getMinMax(ySubInterval[0], ySubInterval[1])
-		const domainY = [minMax.min, minMax.max]
-		const newRangeY = [this.chart.y(domainY[0]), this.chart.y(domainY[1])]
-		const oldRangeY = this.chart.y.range()
-		const scaleY = oldRangeY[0] / (newRangeY[0] - newRangeY[1])
-		const translateY = scaleY * (oldRangeY[1] - newRangeY[1])
-		const ry = d3.scaleLinear().range([this.chart.height, 0]).domain(domainY)
-
-		this.chart.view.attr('transform', `translate(${translateX},${translateY}) scale(${scaleX},${scaleY})`)
-		this.chart.xAxis.setScale(this.chart.rx).axisUp(this.chart.gX)
-		this.chart.yAxis.setScale(ry).axisUp(this.chart.gY)
-	}.bind(this))
-
-	private drawChart(svg: any, data: any[]) {
-		const width = svg.node().parentNode.clientWidth,
-			height = svg.node().parentNode.clientHeight
-		svg.attr('width', width)
-		svg.attr('height', height)
+		const width = div.clientWidth,
+			height = div.clientHeight
 
 		const x = d3.scaleTime().range([0, width])
 		const y = d3.scaleLinear().range([height, 0])
-		const color = d3.scaleOrdinal().domain(['NY', 'SF']).range(['green', 'blue'])
 
-		const xAxis = new axis.MyAxis(axis.Orientation.Bottom, x)
+		const minX = new Date()
+		x.domain([minX, new Date((dataLength - 1) * 86400000 + minX.getTime())])
+		y.domain([-5, 83])
+
+		const xAxis = new MyAxis(Orientation.Bottom, x)
 			.ticks(4)
 			.setTickSize(height)
 			.setTickPadding(8 - height)
 
-		const yAxis = new axis.MyAxis(axis.Orientation.Right, y)
+		const yAxis = new MyAxis(Orientation.Right, y)
 			.ticks(4)
 			.setTickSize(width)
 			.setTickPadding(2 - width)
-
-		const cities = color.domain()
-			.map((name: string) => {
-				return ({
-					name: name,
-					values: data.map((d: any) => +d[name])
-				})
-			})
-
-		this.tree = new segmentTree.SegmentTree(cities, cities[0].values.length, this.buildSegmentTreeTuple)
-
-		x.domain([this.minX, this.maxX])
-		const minMax = this.tree.getMinMax(0, this.tree.size - 1)
-		y.domain([minMax.min, minMax.max])
-
-		const view = svg.append('g')
-			.selectAll('.view')
-			.data(cities)
-			.enter().append('g')
-			.attr('class', 'view')
 
 		const gX = svg.append('g')
 			.attr('class', 'axis')
@@ -127,34 +41,25 @@ export class TimeSeriesChart {
 			.attr('class', 'axis')
 			.call(yAxis.axis.bind(yAxis))
 
-		svg.append('rect')
-			.attr('class', 'zoom')
-			.attr('width', width)
-			.attr('height', height)
-			.call(d3.zoom()
-				.scaleExtent([1, 40])
-				.translateExtent([[0, 0], [width, height]]))
+		let timer = d3timer.timer((elapsed: number) => {
+			const minY = -5
+			const maxY = 83	
+			const k = height / (maxY - minY)
+			const a = -k
+			const b = maxY * k
+			const scaleX = width / dataLength * 2
+			const scaleY = a
+			const translateX = (Math.cos(elapsed / 6500) - 1) * width / 4
+			const translateY = b
+			
+			const zoomTransform = d3.zoomIdentity.translate(translateX, translateY).scale(Math.max(scaleX, scaleY))
+			d3.zoom().transform(d3.selectAll('.zoom'), zoomTransform)
+			const rx = zoomTransform.rescaleX(x)
+			const ry = d3.scaleLinear().range([height, 0]).domain(y.domain())
+			xAxis.setScale(rx).axisUp(gX)
+			yAxis.setScale(ry).axisUp(gY)
 
-		this.chart = {
-			x: x, y: y, rx: x.copy(), ry: y.copy(),
-			xAxis: xAxis, yAxis: yAxis, gX: gX, gY: gY,
-			view: view, data: cities, height: height, color: color
-		}
-	}
-
-	private getZoomIntervalY(xSubInterval: [Date, Date], intervalSize: number): [number, number] {
-		let from = intervalSize
-		let to = 0
-		for (let i = 0; i < intervalSize; i++) {
-			if (this.calcDate(i, this.minX) >= xSubInterval[0] && this.calcDate(i, this.minX) <= xSubInterval[1]) {
-				if (i > to) to = i
-				if (i < from) from = i
-			}
-		}
-		return [from, to]
-	}
-
-	private calcDate(index: number, offset: Date) {
-		return new Date(index * this.stepX + offset.getTime())
+			if (elapsed > 60 * 1000) timer.stop()
+		})
 	}
 }
--- benchmarks/axis-draw-transform/index.ts
+++ benchmarks/axis-draw-transform/index.ts
@@ -1,48 +1,32 @@
 declare const require: Function
-const d3 = require('d3')
+import d3request = require('d3-request')
+import d3selection = require('d3-selection')
 import measureFPS = require('../../measure')
 import draw = require('./draw')
-import segmentTree = require('../../segmentTree')
 
-function getRandom(min: number, max: number) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function buildSegmentTreeTuple(index: number, elements: any): segmentTree.IMinMax {
-	const nyMinValue = isNaN(elements[0].values[index]) ? Infinity : elements[0].values[index]
-	const nyMaxValue = isNaN(elements[0].values[index]) ? -Infinity : elements[0].values[index]
-	const sfMinValue = isNaN(elements[1].values[index]) ? Infinity : elements[1].values[index]
-	const sfMaxValue = isNaN(elements[1].values[index]) ? -Infinity : elements[1].values[index]
-	return { min: Math.min(nyMinValue, sfMinValue), max: Math.max(nyMaxValue, sfMaxValue) }
-}
-
-function drawCharts(data: any[]) {
-	const charts: draw.TimeSeriesChart[] = []
-
-	d3.selectAll('svg').select(function () {
-		const chart = new draw.TimeSeriesChart(d3.select(this), new Date(), 86400000, data, buildSegmentTreeTuple)
-		charts.push(chart)
-	})
-
-	d3.interval(() => {
-		var t = d3.zoomIdentity.translate(getRandom(-500, 500), getRandom(-500, 500)).scale(getRandom(1, 40))
-		charts.forEach(c => c.zoom(t))
-	})
-}
-
-d3
+d3request
 	.csv('ny-vs-sf.csv')
-	.row((d: any) => ({
-		NY: parseFloat(d.NY.split(';')[0]),
-		SF: parseFloat(d.SF.split(';')[0])
-	}))
-	.get((error: any, data: any[]) => {
-		if (error != null) alert('Data can\'t be downloaded or parsed')
-		else drawCharts(data)
-	})
+	.row((d: any) => [
+		parseFloat(d.NY.split(';')[0]),
+		parseFloat(d.SF.split(';')[0])
+	])
+	.get((error: null, data: [number, number]) => {
+		if (error != null)
+		{
+			alert('Data can\'t be downloaded or parsed')
+			return
+		}
 
-measureFPS.measure(3, (fps: any) => {
-	document.getElementById('fps').textContent = fps
-})
+		d3selection.selectAll('svg').each(function () {
+			new draw.TimeSeriesChart(d3selection.select(this), data.length)
+			return this
+		})
+
+		measureFPS.measure(3, (fps: any) => {
+			document.getElementById('fps').textContent = fps
+		})
+
+		measureFPS.measureOnce(60, (fps: number) => {
+			alert(`${window.innerWidth}x${window.innerHeight} FPS = ${fps}`)
+		})
+	})
