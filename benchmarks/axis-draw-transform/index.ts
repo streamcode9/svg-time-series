4de4ce11e57a8e0b7885e57ec981a7f2c8f49928
warau <streamcode9@gmail.com>
2017-01-27 10:54:54 +0000

    Refactor: remove redundant code

--- benchmarks/axis-draw-transform/index.ts
+++ benchmarks/axis-draw-transform/index.ts
@@ -4,28 +4,15 @@ import d3selection = require('d3-selection')
 import measureFPS = require('../../measure')
 import draw = require('./draw')
 
-d3request
-	.csv('ny-vs-sf.csv')
-	.row((d: any) => [
-		parseFloat(d.NY.split(';')[0]),
-		parseFloat(d.SF.split(';')[0])
-	])
-	.get((error: null, data: [number, number]) => {
-		if (error != null) {
-			alert('Data can\'t be downloaded or parsed')
-			return
-		}
+d3selection.selectAll('svg').each(function () {
+	new draw.TimeSeriesChart(d3selection.select(this), 1070)
+	return this
+})
 
-		d3selection.selectAll('svg').each(function () {
-			new draw.TimeSeriesChart(d3selection.select(this), data.length)
-			return this
-		})
+measureFPS.measure(3, (fps: any) => {
+	document.getElementById('fps').textContent = fps
+})
 
-		measureFPS.measure(3, (fps: any) => {
-			document.getElementById('fps').textContent = fps
-		})
-
-		measureFPS.measureOnce(60, (fps: number) => {
-			alert(`${window.innerWidth}x${window.innerHeight} FPS = ${fps}`)
-		})
-	})
+measureFPS.measureOnce(60, (fps: number) => {
+	alert(`${window.innerWidth}x${window.innerHeight} FPS = ${fps}`)
+})
