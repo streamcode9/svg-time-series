82e3ad6e3e4fe398683b392807d9a34e6701426a
Andrii Melnykov <andy.melnikov@gmail.com>
2017-01-28 04:37:26 +0200

    Refactor: less unused code, no more any in benchmarks/std

--- benchmarks/sine-transform-d3/dom-d3.ts
+++ benchmarks/sine-transform-d3/dom-d3.ts
@@ -7,20 +7,15 @@ const delta = 0
 const scale = 0.2
 const data = []
 for (let x = 0; x < 5000; x++) {
-	data.push({x, y: f(x) })
+	data.push(f(x))
 }
-const line: any = d3_line()
-	.x((d: any) => d.x)
-	.y((d: any) => d.y)
+const line = d3_line<number>()
+	.x((y: number, i: number) => i)
+	.y((y: number) => y)
 
 selectAll('path')
 	.datum(data)
 	.attr('d', line)
-	.attr('transform', (d: any, i: number) => {
-		const tx = -delta
-		const ty = i * 50
-		return `translate(${tx}, ${ty}) scale(${scale}, 100)`
-	})
 
 run(100, delta, scale, (delt, scal) => {
 	selectAll('path')
