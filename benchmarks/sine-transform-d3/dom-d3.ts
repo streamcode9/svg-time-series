0058589e17e9fcee4a0bf825c8526141ccabc1ea
Andrii Melnykov <andy.melnikov@gmail.com>
2017-01-28 07:07:02 +0200

    Refactor: fewer signatures in benchmarks/std

--- benchmarks/sine-transform-d3/dom-d3.ts
+++ benchmarks/sine-transform-d3/dom-d3.ts
@@ -10,18 +10,17 @@ for (let x = 0; x < 5000; x++) {
 	data.push(f(x))
 }
 const line = d3_line<number>()
-	.x((y: number, i: number) => i)
-	.y((y: number) => y)
+	.x((y, i) => i)
+	.y((y) => y)
 
-selectAll('path')
+const paths = selectAll('path')
 	.datum(data)
 	.attr('d', line)
 
 run(100, delta, scale, (delt, scal) => {
-	selectAll('path')
-		.attr('transform', (d: any, i: number) => {
-			const tx = -delt
-			const ty = i * 50
-			return `translate(${tx}, ${ty}) scale(${scal}, 100)`
-		})
+	paths.attr('transform', (d, i) => {
+		const tx = -delt
+		const ty = i * 50
+		return `translate(${tx}, ${ty}) scale(${scal}, 100)`
+	})
 })
