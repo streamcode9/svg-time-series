f3345a1b04527e65a34a809d5afba7a3887ddf89
Andrii Melnykov <andy.melnikov@gmail.com>
2017-01-29 16:00:08 +0200

    Internal: tslint warnings

--- benchmarks/common.ts
+++ benchmarks/common.ts
@@ -8,16 +8,22 @@ export function f(x: number) {
 export function run(stepsCount: number = 100, delta: number = 0, scale: number = 0.2, fnRender: (delta: number, scale: number) => void) {
 	let time: number = null
 	let start: number = null
-	function render(timestamp: number) {
-		if (!start) start = timestamp
-		if (time) console.log(timestamp - time)
+	const render = (timestamp: number) => {
+		if (!start) {
+			start = timestamp
+		}
+		if (time) {
+			console.log(timestamp - time)
+		}
 		time = timestamp
 
 		fnRender(delta, scale)
 
 		delta = (timestamp - start) / 20 * (2 / 5)
 		scale = 1 + 0.8 * Math.sin(delta / 50)
-		if (--stepsCount > 0) window.requestAnimationFrame(render)
+		if (--stepsCount > 0) {
+			window.requestAnimationFrame(render)
+		}
 	}
 	window.requestAnimationFrame(render)
 }
