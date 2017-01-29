179666db7523358a0da6e33a6eb1a598c63b2729
Andrii Melnykov <andy.melnikov@gmail.com>
2017-01-29 15:50:16 +0200

    Internal: tslint warnings

--- benchmarks/sine-recreate-dom/dom-first-rendering.ts
+++ benchmarks/sine-recreate-dom/dom-first-rendering.ts
@@ -1,23 +1,26 @@
-﻿import common = require('../common')
+﻿import { f, svg } from '../common'
 
-function createTranslate (x: number, y: number) {
-	const translateTransform = common.svg.createSVGTransform()
+function createTranslate(x: number, y: number) {
+	const translateTransform = svg.createSVGTransform()
 	translateTransform.setTranslate(x, y)
 	return translateTransform
 }
 
-function createScale (x: number, y: number) {
-	const scaleTransform = common.svg.createSVGTransform()
+function createScale(x: number, y: number) {
+	const scaleTransform = svg.createSVGTransform()
 	scaleTransform.setScale(x, y)
 	return scaleTransform
 }
 
 function animate(id: string, yOffset: number) {
-	let delta = 0, scale = 0.2
+	const delta = 0
+	const scale = 0.2
 
 	const path: any = document.getElementById(id)
-	let pathData = [{ type: "M", values: [0, 100] }]
-	for (let x = 0; x < 5000; x++) pathData.push({ type: 'L', values: [x, common.f(x)] })
+	const pathData = [{ type: 'M', values: [0, 100] }]
+	for (let x = 0; x < 5000; x++) {
+		pathData.push({ type: 'L', values: [x, f(x)] })
+	}
 	path.setPathData(pathData);
 
 	const transformations = path.transform.baseVal
@@ -25,9 +28,11 @@ function animate(id: string, yOffset: number) {
 	transformations.appendItem(createScale(scale, 100))
 }
 
-let start = Date.now()
+const start = Date.now()
 function render() {
-	for (let i = 0; i < 9; i++) animate('g' + i, 50 + i * 50)
+	for (let i = 0; i < 9; i++) {
+		animate('g' + i, 50 + i * 50)
+	}
 	window.requestAnimationFrame(() => console.log(Date.now() - start))
 }
 window.requestAnimationFrame(render)
