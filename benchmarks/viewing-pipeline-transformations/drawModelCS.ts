afda76cf51d8cd7c1e585923ed27b607870e6910
genuise <genuisereal@gmail.com>
2017-01-29 18:46:05 +0100

    new: view-pipeline-transformation poc

---
+++ benchmarks/viewing-pipeline-transformations/drawModelCS.ts
@@ -0,0 +1,43 @@
+﻿declare const require: Function
+const d3timer = require('d3-timer')
+import VWTransform = require('./ViewWindowTransform')
+
+export class TimeSeriesChartModelCS {
+    private SVGNode: any
+ 
+    private vwTransform: VWTransform.ViewWindowTransform
+
+    private stepX: number
+
+    constructor(svg: any, minX: Date, stepX: number, cities: any, onPath: Function, dataLength: number) {
+        this.stepX = stepX
+
+        this.SVGNode = svg.node()
+        this.vwTransform = new VWTransform.ViewWindowTransform(this.SVGNode.transform.baseVal)
+
+		const width = svg.node().parentNode.clientWidth,
+			height = svg.node().parentNode.clientHeight
+		svg.attr('width', width)
+        svg.attr('height', height)
+
+        this.vwTransform.setViewPort(width, height)
+        this.vwTransform.setViewWindow(minX.getTime(), this.calcDate(dataLength - 1, minX), 8, 81)
+		
+		const view = svg.append('g')
+			.selectAll('.view')
+			.data(cities)
+			.enter().append('g')
+			.attr('class', 'view')
+
+		onPath(view.append('path'))
+    }
+
+    private calcDate(index: number, offset: Date) {
+        const d = new Date(index * this.stepX + offset.getTime()).getTime()
+        return d
+    }
+}
+
+
+
+
---
+++ benchmarks/viewing-pipeline-transformations/index.ts
@@ -0,0 +1,57 @@
+﻿declare const require: Function
+const d3request = require('d3-request')
+const d3shape = require('d3-shape')
+const d3selection = require('d3-selection')
+import measureFPS = require('../../measure')
+import draw = require('./draw')
+import drawModelCS = require('./drawModelCS')
+
+const startDate = new Date()
+d3request
+	.csv('ny-vs-sf.csv')
+	.row((d: any) => [
+		parseFloat(d.NY.split(';')[0]),
+		parseFloat(d.SF.split(';')[0])
+	])
+	.get((error: any, data: any[]) => {
+		if (error != null)
+		{
+			alert('Data can\'t be downloaded or parsed')
+			return
+		}
+
+		const onPath = (path: any) => {
+			 path.attr('d', (cityIdx: number) =>
+				d3shape.line()
+                     .defined((d: number[]) => d[cityIdx])
+                     .x((d: number[], i: number) => i)
+					.y((d: number[]) => d[cityIdx])
+					.call(null, data)
+			)
+		}
+
+        const onPathModel = (path: any) => {
+            path.attr('d', (cityIdx: number) =>
+                d3shape.line()
+                    .defined((d: number[]) => d[cityIdx])
+                    .x((d: number[], i: number) => calcDate(i, startDate, 86400000))
+                    .y((d: number[]) => d[cityIdx])
+                    .call(null, data)
+            )
+        }
+
+        d3selection.selectAll('svg#default').select(function () {
+            new draw.TimeSeriesChart(d3selection.select(this), 0, 1, [0, 1], onPath, data.length)
+		})
+        d3selection.selectAll('svg#model').select(function () {
+            new drawModelCS.TimeSeriesChartModelCS(d3selection.select(this), startDate, 86400000, [0, 1], onPathModel, data.length)
+        })
+    })
+
+function calcDate(index: number, offset: Date, step: number) {
+    const d = new Date(index * step + offset.getTime()).getTime()
+    return d
+}
+
+
+
