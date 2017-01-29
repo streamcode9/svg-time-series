126bbcf81cfbf51f4dd398429c000f2ded6d918c
Andrii Melnykov <andy.melnikov@gmail.com>
2017-01-29 15:11:43 +0200

    Refactor: benchmarks/stq uses WindowViewTransform

--- benchmarks/segment-tree-queries/draw.ts
+++ benchmarks/segment-tree-queries/draw.ts
@@ -1,8 +1,10 @@
-﻿import d3timer = require('d3-timer')
-import { BaseType, Selection } from 'd3-selection'
-import { IMinMax, SegmentTree } from '../../segmentTree'
+﻿import { BaseType, Selection } from 'd3-selection'
 
-function buildSegmentTreeTuple(index: number, elements: number[][]): IMinMax {
+import { IMinMax, SegmentTree } from '../../segmentTree'
+import { animateBench, animateCosDown } from '../bench'
+import { ViewWindowTransform } from '../viewing-pipeline-transformations/ViewWindowTransform'
+
+function buildSegmentTreeTuple(index: number, elements: number[][]) : IMinMax {
 	const nyMinValue = isNaN(elements[index][0]) ? Infinity : elements[index][0]
 	const nyMaxValue = isNaN(elements[index][0]) ? -Infinity : elements[index][0]
 	const sfMinValue = isNaN(elements[index][1]) ? Infinity : elements[index][1]
@@ -14,36 +16,19 @@ export class TimeSeriesChart {
 	constructor( svg: Selection<BaseType, {}, HTMLElement, any>, data: number[][]) {
 		const node: SVGSVGElement = svg.node() as SVGSVGElement
 		const div: HTMLElement = node.parentNode as HTMLElement
+		const viewNode: SVGGElement = svg.select('g').node() as SVGGElement
 
-		const width = div.clientWidth
-		const height = div.clientHeight
+		const dataLength = data.length
+		const tree = new SegmentTree(data, dataLength, buildSegmentTreeTuple)
 
-		const view = svg.select('g')
+		const t = new ViewWindowTransform(viewNode.transform.baseVal)
+		t.setViewPort(div.clientWidth, div.clientHeight)
 
-		const tree = new SegmentTree(data, data.length, buildSegmentTreeTuple)
-
-		const timer = d3timer.timer((elapsed: number) => {
-			const minMax = tree.getMinMax(0, tree.size - 1)
-			const minY = minMax.min
-			const maxY = minMax.max
-			const k = height / (maxY - minY)
-			// conceptually viewPortY = a * temperature + b
-			// and actually viewPortY = scaleY * lineY + translateY
-
-			// actually it's better to explain what's going on
-			// with compositions of rangeTransform(inMin, inMax, outMin, outMax)
-			const a = -k
-			const b = maxY * k
-			const scaleX = width / data.length * 2
-			const scaleY = a
-			const translateX = (Math.cos(elapsed / 6500) - 1) * width / 4
-			const translateY = b
-
-			view.attr('transform', `translate(${translateX},${translateY}) scale(${scaleX},${scaleY})`)
-
-			if (elapsed > 60 * 1000) {
-				timer.stop()
-			}
+		animateBench((elapsed: number) => {
+			const minX = animateCosDown(dataLength / 2, 0, elapsed)
+			const maxX = minX + dataLength / 2
+			const {min, max} = tree.getMinMax(minX, maxX)
+			t.setViewWindow(minX, maxX, min, max)
 		})
 	}
 }
