60a4bec423dfa9951bb3406a32aa2e1c25a1314c
Alex <streamcode9@gmail.com>
2017-02-04 22:46:40 +0200

    New: path-recreate-dom benchmark Close #88

---
+++ benchmarks/path-recreate-dom/draw.ts
@@ -0,0 +1,34 @@
+﻿import {  BaseType, selectAll, Selection } from 'd3-selection'
+
+import { animateBench, animateCosDown } from '../bench'
+import { ViewWindowTransform } from '../../ViewWindowTransform'
+
+export class TimeSeriesChart {
+	constructor(
+		svg: Selection<BaseType, {}, HTMLElement, any>,
+		dataLength: number,
+		drawLine: (element: any, idx: number, off: number) => void) {
+		const node: SVGSVGElement = svg.node() as SVGSVGElement
+		const div: HTMLElement = node.parentNode as HTMLElement
+		const viewNode: SVGGElement = svg.select('g').node() as SVGGElement
+		const t = new ViewWindowTransform(viewNode.transform.baseVal)
+		t.setViewPort(div.clientWidth, div.clientHeight)
+
+		const minY = -5
+		const maxY = 83
+		const minX = animateCosDown(dataLength / 2, 0, 0)
+		const maxX = minX + dataLength / 2
+		t.setViewWindow(minX, maxX, minY, maxY)
+
+		const paths: Selection<any, any, any, any> = svg.select('g.view').selectAll('path')
+
+		let off = 0
+		animateBench((elapsed: number) => {
+			// Redraw paths
+			paths.each(function(cityIdx: number) {
+				drawLine(this, cityIdx, off)
+			})
+			off += 1
+		})
+	}
+}
---
+++ benchmarks/path-recreate-dom/index.ts
@@ -0,0 +1,39 @@
+﻿import { select, selectAll } from 'd3-selection'
+import { measureAll, onCsv } from '../bench'
+import { TimeSeriesChart } from './draw'
+
+onCsv((data) => {
+	const dataLength = data.length
+
+	const drawLine = (pathElement: any, cityIdx: number, off: number) => {
+		const idx = (i: number) => (i + off) % dataLength
+
+		let pathData: any = []
+		let previousPointIsValid = true
+		data.forEach((d: number[], i: number, arr: number[][]) => {
+			const y = arr[idx(i)][cityIdx]
+			const currentPointIsValid = !isNaN(y)
+
+			if (!previousPointIsValid && currentPointIsValid) {
+				pathData.push({ type: 'M', values: [i, y]})
+			}
+
+			pathData.push({ type: i == 0 || !currentPointIsValid ? 'M' : 'L', values: [i, currentPointIsValid ? y : 0]})
+
+			previousPointIsValid = currentPointIsValid
+		})
+
+		pathElement.setPathData(pathData)
+	}
+
+	const path = selectAll('g.view')
+		.selectAll('path')
+		.data([0, 1])
+		.enter().append('path')
+
+	selectAll('svg').each(function() {
+		return new TimeSeriesChart(select(this), dataLength, drawLine)
+	})
+
+	measureAll()
+})
