50c63b07a7cc90bcaa7f548779d10b6530e90301
Alex <streamcode9@gmail.com>
2017-02-05 16:29:36 +0200

    Internal: fix empty data handling #88

--- benchmarks/path-recreate-dom/index.ts
+++ benchmarks/path-recreate-dom/index.ts
@@ -13,32 +13,44 @@ onCsv((data) => {
 		const currentPointIsValid = [!isNaN(y0), !isNaN(y1)]
 
 		if (!previousPointIsValid[0] && currentPointIsValid[0]) {
-			pathsData[0].push({ type: 'M', values: [i, y0]})
+			pathsData[0][i - 1].values[1] = y0
 		}
 
 		if (!previousPointIsValid[1] && currentPointIsValid[1]) {
-			pathsData[1].push({ type: 'M', values: [i, y1]})
+			pathsData[1][i - 1].values[1] = y1
 		}
 
-		pathsData[0].push({ type: i == 0 || !currentPointIsValid[0] ? 'M' : 'L', values: [i, currentPointIsValid[0] ? y0 : 0], isValid: currentPointIsValid[0]})
-		pathsData[1].push({ type: i == 0 || !currentPointIsValid[1] ? 'M' : 'L', values: [i, currentPointIsValid[1] ? y1 : 0], isValid: currentPointIsValid[1]})
+		pathsData[0].push({ type: currentPointIsValid[0] ? 'L' : 'M', values: [i, currentPointIsValid[0] ? y0 : 0]})
+		pathsData[1].push({ type: currentPointIsValid[1] ? 'L' : 'M', values: [i, currentPointIsValid[1] ? y1 : 0]})
 
 		previousPointIsValid = currentPointIsValid
 	})
 
+	if (!previousPointIsValid[0]) {
+		pathsData[0][dataLength - 1].values[1] = pathsData[0][0].values[1]
+	}
+
+	if (!previousPointIsValid[1]) {
+		pathsData[1][dataLength - 1].values[1] = pathsData[1][0].values[1]
+	}
+
 	const drawLine = (pathElement: any, cityIdx: number) => {
 		// Push new point
 		let newData: any = Object.assign({}, pathsData[cityIdx][0])
-		newData.type = newData.isValid ? 'L' : 'M'
+		newData.values = newData.values.map((_: any) => _)
 		pathsData[cityIdx].push(newData)
 
 		// Remove first point
 		pathsData[cityIdx].shift()
 
-		// Set path start point
-		let firstPoint: any = Object.assign({}, pathsData[cityIdx][0])
-		pathsData[cityIdx].unshift(firstPoint)
-		pathsData[cityIdx][0].type = 'M'
+		// Set temporary start point
+		const needToAddStartPoint = pathsData[cityIdx][0].type == 'L'
+		if (needToAddStartPoint) {
+			let firstPoint: any = Object.assign({}, pathsData[cityIdx][0])
+			firstPoint.type = 'M'
+			firstPoint.values = firstPoint.values.map((_: any) => _)
+			pathsData[cityIdx].unshift(firstPoint)
+		}
 
 		// Recalculate indexes
 		pathsData[cityIdx] = pathsData[cityIdx].map((d: any, i: number) => {
@@ -50,7 +62,7 @@ onCsv((data) => {
 		pathElement.setPathData(pathsData[cityIdx])
 
 		// Remove temporary start point
-		pathsData[cityIdx].shift()
+		if (needToAddStartPoint) pathsData[cityIdx].shift()
 	}
 
 	const path = selectAll('g.view')
