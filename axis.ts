c5558818bd695c8655c2ef7fdc0ec819281eb138
Alexander <streamcode9@gmail.com>
2018-04-26 22:54:42 +0300

    New: add second Y axis. 1st iteration - just make copy of existing Y axis, but align to another side of drawing area. #102

--- axis.ts
+++ axis.ts
@@ -50,19 +50,22 @@ export class MyAxis {
 			format: any = this.tickFormat == null ? (this.scale.tickFormat ? this.scale.tickFormat.apply(this.scale, this.tickArguments) : identity) : this.tickFormat,
 			spacing: any = Math.max(this.tickSizeInner, 0) + this.tickPadding,
 			transform: any = this.orient === Orientation.Top || this.orient === Orientation.Bottom ? translateX : translateY,
-			position = (this.scale.bandwidth ? center : identity)(this.scale.copy())
+			position = (this.scale.bandwidth ? center : identity)(this.scale.copy()),
+			isY = this.orient === Orientation.Left || this.orient === Orientation.Right
 		let tick = context.selectAll('.tick').data(values, this.scale).order(),
 			tickExit = tick.exit(),
 			tickEnter = tick.enter().append('g').attr('class', 'tick'),
 			line = tick.select('line'),
-			text = tick.select('text'),
+			text1 = tick.select('text'),
+			text2 = isY ? tick.select('text') : tick.select(),
 			k = this.orient === Orientation.Top || this.orient === Orientation.Left ? -1 : 1
 		let x = ''
-		const y = this.orient === Orientation.Left || this.orient === Orientation.Right ? (x = 'x', 'y') : (x = 'y', 'x')
+		const y = isY ? (x = 'x', 'y') : (x = 'y', 'x')
 
 		tick = tick.merge(tickEnter)
 		line = line.merge(tickEnter.append('line').attr(x + '2', k * this.tickSizeInner))
-		text = text.merge(tickEnter.append('text').attr(x, k * spacing))
+		text1 = text1.merge(tickEnter.append('text').attr(x, k * spacing))
+		text2 = isY ? text2.merge(tickEnter.append('text').attr(x, k * spacing)) : text2
 
 		tickExit.remove()
 
@@ -73,11 +76,20 @@ export class MyAxis {
 			.attr(y + '1', 0.5)
 			.attr(y + '2', 0.5)
 
-		text
+		text1
 			.attr(x, k * spacing)
 			.attr(y, 3)
 			.attr('dy', this.orient === Orientation.Top ? '0em' : this.orient === Orientation.Bottom ? '.41em' : '.62em')
 			.text(format)
+			.attr('class', `tick__${y}-first`)
+
+		text2
+			.attr(x, this.tickSizeInner - spacing)
+			.attr(y, 3)
+			.attr('dy', '.41em')
+			.attr('text-anchor', 'end')
+			.text(format)
+			.attr('class', `tick__${y}-second`)
 
 		context
 			.attr('text-anchor', this.orient === Orientation.Right ? 'start' : this.orient === Orientation.Left ? 'end' : 'middle')
@@ -90,18 +102,23 @@ export class MyAxis {
 			spacing = Math.max(this.tickSizeInner, 0) + this.tickPadding,
 			transform = this.orient === Orientation.Top || this.orient === Orientation.Bottom ? translateX : translateY,
 			position = (this.scale.bandwidth ? center : identity)(this.scale.copy()),
+			isY = this.orient === Orientation.Left || this.orient === Orientation.Right,
 			k = this.orient === Orientation.Top || this.orient === Orientation.Left ? -1 : 1
 		let tick = context.selectAll('.tick').data(values, this.scale).order(),
 			tickExit = tick.exit(),
 			tickEnter = tick.enter().append('g').attr('class', 'tick'),
-			line = tick.select('line'),
-			text = tick.select('text')
+			line = tick.select('line')
+
 		let x = ''
-		const y = this.orient === Orientation.Left || this.orient === Orientation.Right ? (x = 'x', 'y') : (x = 'y', 'x')
+		const y = isY ? (x = 'x', 'y') : (x = 'y', 'x')
+
+		let text1 = tick.select(`tick__${y}-first`),
+			text2 = isY ? tick.select(`tick__${y}-second`) : tick.select()
 
 		tick = tick.merge(tickEnter)
 		line = line.merge(tickEnter.append('line').attr(x + '2', k * this.tickSizeInner))
-		text = text.merge(tickEnter.append('text').attr(x, k * spacing))
+		text1 = text1.merge(tickEnter.append('text').attr(x, k * spacing))
+		text2 = isY ? text2.merge(tickEnter.append('text').attr(x, k * spacing)) : text2
 
 		tickExit.remove()
 
@@ -112,11 +129,20 @@ export class MyAxis {
 			.attr(y + '1', 0.5)
 			.attr(y + '2', 0.5)
 
-		text
+		text1
 			.attr(x, k * spacing)
 			.attr(y, 0.5)
 			.attr('dy', this.orient === Orientation.Top ? '0em' : this.orient === Orientation.Bottom ? '.41em' : '.62em')
 			.text(format)
+			.attr('class', `tick__${y}-first`)
+
+		text2
+			.attr(x, this.tickSizeInner - spacing)
+			.attr(y, 3)
+			.attr('dy', '.41em')
+			.attr('text-anchor', 'end')
+			.text(format)
+			.attr('class', `tick__${y}-second`)
 	}
 
 	setScale(_: any) { return this.scale = _, this }
