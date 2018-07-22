b1b83e33d111547a536eb79ed354f33a38f66f24
Alexander <streamcode9@gmail.com>
2018-07-22 22:56:02 +0300

    Internal: draw texts for Y axis #102

--- axis.ts
+++ axis.ts
@@ -47,10 +47,22 @@ export class MyAxis {
 		this.tickPadding = 3
 	}
 
+	createValues(scale1: any, scale2?: any): Array<Array<any>> {
+		const createValuesFromScale = (scale: any): Array<any> => scale.ticks ? scale.ticks.apply(scale, this.tickArguments) : scale.domain()
+
+		const values1 = createValuesFromScale(scale1);
+		const values2 = scale2 ? createValuesFromScale(scale2) : [];
+		return values1.map((v: any, i: number) => [v, scale2 ? values2[i] : null])
+	}
+
+	createFormat(scale: any, columnIdx: number): (tuple: Array<any>) => any {
+		const formatValue: (value: any) => any = (scale: any) => scale.tickFormat ? scale.tickFormat.apply(scale, this.tickArguments) : identity
+		return (tuple: Array<any>) => (formatValue(scale)(tuple[columnIdx]))
+	}
+
 	axis(context: any) {
-		const isY = this.scale2 ? true : false
-		const values: any = this.tickValues == null ? (this.scale1.ticks ? this.scale1.ticks.apply(this.scale1, this.tickArguments) : this.scale1.domain()) : this.tickValues,
-			format: any = this.tickFormat == null ? (this.scale1.tickFormat ? this.scale1.tickFormat.apply(this.scale1, this.tickArguments) : identity) : this.tickFormat,
+		const values = this.createValues(this.scale1, this.scale2),
+			formats = this.scale2 ? [this.createFormat(this.scale1, 0), this.createFormat(this.scale2, 1)] : [this.createFormat(this.scale1, 0)],
 			spacing: any = Math.max(this.tickSizeInner, 0) + this.tickPadding,
 			transform: any = this.orient === Orientation.Top || this.orient === Orientation.Bottom ? translateX : translateY,
 			position = (this.scale1.bandwidth ? center : identity)(this.scale1.copy())
@@ -65,22 +77,25 @@ export class MyAxis {
 
 		tick = tick.merge(tickEnter)
 		line = line.merge(tickEnter.append('line').attr(x + '2', k * this.tickSizeInner))
-		text = text.merge(tickEnter.append('text').attr(x, k * spacing))
+
+		const createText = () => text.merge(tickEnter.append('text').attr(x, k * spacing))
+		const texts = this.scale2 ? [createText(), createText()] : [createText()]
 
 		tickExit.remove()
 
-		tick.attr('transform', (d: any) => transform(position, position, d))
+		// fix needs there, we must not hard-code index
+		tick.attr('transform', (d: any) => transform(position, position, d[0]))
 
 		line
 			.attr(x + '2', k * this.tickSizeInner)
 			.attr(y + '1', 0.5)
 			.attr(y + '2', 0.5)
 
-		text
-			.attr(x, k * spacing)
+		texts.map((txt: any, i: number) =>
+			txt.attr(x, k * spacing)
 			.attr(y, 3)
 			.attr('dy', this.orient === Orientation.Top ? '0em' : this.orient === Orientation.Bottom ? '.41em' : '.62em')
-			.text(format)
+			.text(formats[i]))
 
 		context
 			.attr('text-anchor', this.orient === Orientation.Right ? 'start' : this.orient === Orientation.Left ? 'end' : 'middle')
@@ -88,8 +103,8 @@ export class MyAxis {
 	}
 
 	axisUp(context: any) {
-		const values = this.tickValues == null ? (this.scale1.ticks ? this.scale1.ticks.apply(this.scale1, this.tickArguments) : this.scale1.domain()) : this.tickValues,
-			format = this.tickFormat == null ? (this.scale1.tickFormat ? this.scale1.tickFormat.apply(this.scale1, this.tickArguments) : identity) : this.tickFormat,
+		const values = this.createValues(this.scale1, this.scale2),
+			formats = this.scale2 ? [this.createFormat(this.scale1, 0), this.createFormat(this.scale2, 1)] : [this.createFormat(this.scale1, 0)],
 			spacing = Math.max(this.tickSizeInner, 0) + this.tickPadding,
 			transform = this.orient === Orientation.Top || this.orient === Orientation.Bottom ? translateX : translateY,
 			position = (this.scale1.bandwidth ? center : identity)(this.scale1.copy()),
@@ -105,22 +120,25 @@ export class MyAxis {
 
 		tick = tick.merge(tickEnter)
 		line = line.merge(tickEnter.append('line').attr(x + '2', k * this.tickSizeInner))
-		text = text.merge(tickEnter.append('text').attr(x, k * spacing))
+
+		const createText = () => text.merge(tickEnter.append('text').attr(x, k * spacing))
+		const texts = this.scale2 ? [createText(), createText()] : [createText()]
 
 		tickExit.remove()
 
-		tick.attr('transform', (d: any) => transform(position, position, d))
+		// fix is needed there
+		tick.attr('transform', (d: any) => transform(position, position, d[0]))
 
 		line
 			.attr(x + '2', k * this.tickSizeInner)
 			.attr(y + '1', 0.5)
 			.attr(y + '2', 0.5)
 
-		text
-			.attr(x, k * spacing)
-			.attr(y, 0.5)
-			.attr('dy', this.orient === Orientation.Top ? '0em' : this.orient === Orientation.Bottom ? '.41em' : '.62em')
-			.text(format)
+		texts.map((txt: any, i: number) =>
+			txt.attr(x, k * spacing)
+				.attr(y, 3)
+				.attr('dy', this.orient === Orientation.Top ? '0em' : this.orient === Orientation.Bottom ? '.41em' : '.62em')
+				.text(formats[i]))
 	}
 
 	setScale(scale1: any, scale2?: any) {
