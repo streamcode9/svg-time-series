a4f797c5308785e30879e1d63d97dd7fa13111f6
nponeccop <andy.melnikov@gmail.com>
2024-08-25 22:02:40 +0000

    refactor: delete unused typings

--- typings/assert.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "assert" {
-  function assert(value: any, message?: string): void;
-
-  namespace assert {
-    export function ok(value: any, message?: string): void;
-  }
-
-  export = assert;
-}
