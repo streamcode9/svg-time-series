8a552533206bd29cd0036fd5a26c822263e622d1
nponeccop <andy.melnikov@gmail.com>
2024-08-28 21:29:50 +0000

    chore: improve vite.config

--- svg-time-series/vite.config.ts
+++ svg-time-series/vite.config.ts
@@ -1,18 +1,18 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { UserConfig, defineConfig } from "vite";
 import nodeExternals from "rollup-plugin-node-externals";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/draw.ts"),
+      entry: "src/draw.ts",
       fileName: "index",
       formats: ["es"],
     },
     rollupOptions: {
       plugins: [nodeExternals()],
     },
+    sourcemap: true,
   },
   plugins: [dts({ rollupTypes: true })],
-});
+}) satisfies UserConfig;
