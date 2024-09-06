f45d46b4bf3674301d6d93a2373100c463ad1069
nponeccop <andy.melnikov@gmail.com>
2024-09-06 01:18:56 +0000

    refactor: generalize the segment tree

--- svg-time-series/src/segmentTree.ts
+++ svg-time-series/src/segmentTree.ts
@@ -1,3 +1,5 @@
+import { SegmentTreeHalf } from './segmentTreeHalf';
+
 export interface IMinMax {
   readonly min: number;
   readonly max: number;
@@ -7,16 +9,20 @@ function buildMinMax(fst: Readonly<IMinMax>, snd: Readonly<IMinMax>): IMinMax {
   return { min: Math.min(fst.min, snd.min), max: Math.max(fst.max, snd.max) } as const;
 }
 
+const minMaxIdentity: IMinMax = {
+  min: Infinity,
+  max: -Infinity
+};
+
 function assertOk(cond: boolean, message: string): asserts cond {
   if (!cond) {
     throw new Error(message);
   }
 }
 
-export class SegmentTree<T = any> {
+export class SegmentTree<T = [number, number]> {
   private readonly size: number;
-  private readonly tree: IMinMax[];
-  private readonly buildTuple: (elementIndex: number, elements: ReadonlyArray<T>) => IMinMax;
+  private tree: SegmentTreeHalf<IMinMax>;
 
   constructor(
     data: ReadonlyArray<T>,
@@ -24,85 +30,29 @@ export class SegmentTree<T = any> {
     buildTuple: (elementIndex: number, elements: ReadonlyArray<T>) => IMinMax,
   ) {
     this.size = dataSize;
-    this.tree = new Array(this.size * 4);
-    this.buildTuple = buildTuple;
+    const minMaxData: IMinMax[] = new Array(this.size);
 
-    const build = (
-      tree: IMinMax[],
-      values: ReadonlyArray<T>,
-      i: number,
-      left: number,
-      right: number,
-    ): void => {
-      if (left === right) {
-        tree[i] = this.buildTuple(left, values);
-      } else {
-        const middle = Math.floor((left + right) / 2);
-        build(tree, values, 2 * i, left, middle);
-        build(tree, values, 2 * i + 1, middle + 1, right);
-        tree[i] = buildMinMax(tree[2 * i], tree[2 * i + 1]);
-      }
-    };
-    build(this.tree, data, 1, 0, this.size - 1);
+    for (let i = 0; i < this.size; i++) {
+      minMaxData[i] = buildTuple(i, data);
+    }
+
+    this.tree = new SegmentTreeHalf(minMaxData, buildMinMax, minMaxIdentity);
   }
 
   getMinMax(fromPosition: number, toPosition: number): IMinMax {
     assertOk(
-      fromPosition >= 0 && toPosition <= this.size,
+      fromPosition >= 0 && toPosition <= this.size && fromPosition <= toPosition,
       "Range is not valid",
     );
-    const getMinMax = (
-      tree: ReadonlyArray<IMinMax>,
-      i: number,
-      left: number,
-      right: number,
-      from: number,
-      to: number,
-    ): IMinMax => {
-      if (from > to) {
-        return { min: Infinity, max: -Infinity } as const;
-      }
-      if (from === left && to === right) {
-        return tree[i];
-      }
-      const middle = Math.floor((left + right) / 2);
-      return buildMinMax(
-        getMinMax(tree, i * 2, left, middle, from, Math.min(to, middle)),
-        getMinMax(
-          tree,
-          i * 2 + 1,
-          middle + 1,
-          right,
-          Math.max(from, middle + 1),
-          to,
-        ),
-      );
-    };
-    return getMinMax(this.tree, 1, 0, this.size - 1, fromPosition, toPosition);
+    return this.tree.query(fromPosition, toPosition);
   }
 
   update(positionToUpdate: number, newValue: Readonly<IMinMax>): void {
-    const update = (
-      tree: IMinMax[],
-      i: number,
-      left: number,
-      right: number,
-      position: number,
-      newTuple: Readonly<IMinMax>,
-    ): void => {
-      if (left === right) {
-        tree[i] = newTuple;
-        return;
-      }
+    assertOk(
+      positionToUpdate >= 0 && positionToUpdate < this.size,
+      "Position is not valid",
+    );
 
-      const middle = Math.floor((left + right) / 2);
-      if (position <= middle) {
-        update(tree, i * 2, left, middle, position, newTuple);
-      } else {
-        update(tree, i * 2 + 1, middle + 1, right, position, newTuple);
-      }
-      tree[i] = buildMinMax(tree[i * 2], tree[i * 2 + 1]);
-    };
-    update(this.tree, 1, 0, this.size - 1, positionToUpdate, newValue);
+    this.tree.update(positionToUpdate, newValue);
   }
 }
---
+++ svg-time-series/src/segmentTreeHalf.ts
@@ -0,0 +1,61 @@
+export type Operator<T> = (a: T, b: T) => T;
+
+export class SegmentTreeHalf<T> {
+  private size: number;
+  private tree: T[];
+  private op: Operator<T>;
+  private identity: T;
+
+  constructor(
+    data: T[],
+    op: Operator<T>,
+    identity: T
+  ) {
+    this.size = data.length;
+    this.tree = new Array(this.size * 2);
+    this.op = op;
+    this.identity = identity;
+
+    // Build the tree
+    for (let i = 0; i < this.size; i++) {
+      this.tree[this.size + i] = data[i];
+    }
+    for (let i = this.size - 1; i > 0; i--) {
+      this.tree[i] = this.op(this.tree[i * 2], this.tree[i * 2 + 1]);
+    }
+  }
+
+  update(index: number, value: T): void {
+    let i = index + this.size;
+    this.tree[i] = value;
+    while (i > 1) {
+      i = Math.floor(i / 2);
+      this.tree[i] = this.op(this.tree[i * 2], this.tree[i * 2 + 1]);
+    }
+  }
+
+  query(left: number, right: number): T {
+    left += this.size;
+    right += this.size;
+    let result = this.identity;
+
+    while (left < right) {
+      if (left % 2 === 1) {
+        result = this.op(result, this.tree[left]);
+        left++;
+      }
+      if (right % 2 === 0) {
+        result = this.op(result, this.tree[right]);
+        right--;
+      }
+      left = Math.floor(left / 2);
+      right = Math.floor(right / 2);
+    }
+
+    if (left === right) {
+      result = this.op(result, this.tree[left]);
+    }
+
+    return result;
+  }
+}
