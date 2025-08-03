da5d244fc21acd363a3aced6627100f9dc794c77
nponeccop <andy.melnikov@gmail.com>
2025-08-03 16:20:15 +0200

    Add CI workflow with lint and tests

--- eslint.config.js
+++ eslint.config.js
@@ -8,4 +8,14 @@ export default tsEslint.config(
   eslint.configs.recommended,
   ...tsEslint.configs.recommended,
   prettierConfig,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "prefer-const": "off",
+      "prefer-spread": "off",
+    },
+  },
 );
