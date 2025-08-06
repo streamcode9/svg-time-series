a416a1c020a8325438041c791d1a9c1a6e57ac86
nponeccop <andy.melnikov@gmail.com>
2025-08-06 12:19:35 +0200

    feat: add chart dispose handling

--- svg-time-series/src/chart/interaction.test.ts
+++ svg-time-series/src/chart/interaction.test.ts
@@ -7,6 +7,7 @@ import { AR1Basis } from "../math/affine.ts";
 import { ChartData } from "./data.ts";
 import { setupRender } from "./render.ts";
 import { setupInteraction } from "./interaction.ts";
+import { TimeSeriesChart } from "../draw.ts";
 
 class Matrix {
   constructor(
@@ -259,5 +260,56 @@ describe("chart interaction", () => {
       createChart([]);
       vi.runAllTimers();
     }).toThrow();
+  });
+
+  it("dispose removes event listeners and DOM elements", () => {
+    const parent = document.createElement("div");
+    Object.defineProperty(parent, "clientWidth", {
+      value: 10,
+      configurable: true,
+    });
+    Object.defineProperty(parent, "clientHeight", {
+      value: 10,
+      configurable: true,
+    });
+    const svgEl = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    parent.appendChild(svgEl);
+    const legend = document.createElement("div");
+    legend.innerHTML =
+      '<span class="chart-legend__time"></span>' +
+      '<span class="chart-legend__green_value"></span>' +
+      '<span class="chart-legend__blue_value"></span>';
+
+    const mouseMoveHandler = vi.fn();
+
+    const chart = new TimeSeriesChart(
+      select(svgEl) as any,
+      select(legend) as any,
+      0,
+      1,
+      [
+        [0, 0],
+        [1, 1],
+      ],
+      (i, arr) => ({ min: arr[i][0], max: arr[i][0] }),
+      (i, arr) => ({ min: arr[i][1]!, max: arr[i][1]! }),
+      () => {},
+      mouseMoveHandler,
+    );
+
+    const zoomRect = svgEl.querySelector("rect.zoom") as SVGRectElement;
+    expect(zoomRect).not.toBeNull();
+
+    zoomRect.dispatchEvent(new MouseEvent("mousemove"));
+    expect(mouseMoveHandler).toHaveBeenCalledTimes(1);
+
+    chart.dispose();
+
+    expect(svgEl.querySelector("rect.zoom")).toBeNull();
+    expect(svgEl.querySelectorAll("circle").length).toBe(0);
+
+    mouseMoveHandler.mockClear();
+    zoomRect.dispatchEvent(new MouseEvent("mousemove"));
+    expect(mouseMoveHandler).not.toHaveBeenCalled();
   });
 });
