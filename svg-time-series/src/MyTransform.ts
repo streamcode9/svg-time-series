9ef6af07b5176f1b4b3b945df370a648a9de3047
nponeccop <andy.melnikov@gmail.com>
2025-08-06 13:39:57 +0200

    refactor: remove MyTransform helper

--- svg-time-series/src/MyTransform.ts
+++
@@ -1,9 +0,0 @@
-import { ViewportTransform } from "./ViewportTransform.ts";
-import { updateNode } from "./viewZoomTransform.ts";
-
-export function applyViewportTransform(
-  node: SVGGraphicsElement,
-  transform: ViewportTransform,
-): void {
-  updateNode(node, transform.matrix);
-}
