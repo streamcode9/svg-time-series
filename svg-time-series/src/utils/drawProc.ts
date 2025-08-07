6b313f010c96060afc7afd7e0f67cac63404f399
nponeccop <andy.melnikov@gmail.com>
2025-08-07 16:17:31 +0200

    fix: use latest params in draw proc

--- svg-time-series/src/utils/drawProc.ts
+++ svg-time-series/src/utils/drawProc.ts
@@ -4,13 +4,15 @@ export function drawProc<F extends (...args: unknown[]) => void>(
   f: F,
 ): (...args: Parameters<F>) => void {
   let requested = false;
+  let latestParams: Parameters<F>;
 
   return (...params: Parameters<F>) => {
+    latestParams = params;
     if (!requested) {
       requested = true;
       runTimeout(() => {
         requested = false;
-        f(...params);
+        f(...latestParams);
       });
     }
   };
