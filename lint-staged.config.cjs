8d53c6123a3c34d7a5c64e8ddc34fb1be2707c19
nponeccop <andy.melnikov@gmail.com>
2025-08-08 20:27:04 +0200

    chore: enforce zero lint warnings

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "*.{js,jsx,ts,tsx}": ["eslint --fix --max-warnings=0", "prettier --write"],
   "*.{cjs,mjs,json,css,md}": ["prettier --write"],
   "*.{yml,yaml}": ["prettier --write"],
 };
